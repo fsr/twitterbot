db839d83a8c5c0c04f8452e710916aae410c8961
Kilian Koeltzsch <me@kilian.io>
2016-05-11 16:26:52 +0200

    Add PathKit and Yaml as dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "twitterbot"
+    name: "twitterbot",
+    dependencies: [
+        .Package(url: "https://github.com/kylef/PathKit", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/behrang/YamlSwift", majorVersion: 1, minor: 4)
+    ]
 )
